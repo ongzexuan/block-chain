33b2f90af39d7922df5aab7a32b1a63873549a6e
Ze Xuan Ong <zexuan@stashaway.com>
2018-09-21 19:25:10 -0400

    Added base skills

---
+++ skills/general.js
@@ -0,0 +1,31 @@
+
+module.exports = function(controller, dialogflow) {
+
+    // Function to end all convos (prevent spillover contexts)
+    function endExistingConvos(user) {
+        console.log('Attempting to end existing convos')
+        var tasks = controller.tasks
+        for (let task of tasks) {
+            for (let convo of task.convos) {
+                if (convo.isActive() && convo.source_message.user == user) {
+                    convo.stop()
+                    console.log('Stopped a convo!')
+                }
+            }
+        }
+
+    }
+
+    controller.hears(['get_human_help'], 'facebook_postback', function(bot, message) {
+        console.log("Received request for get_human_help postback!");
+        endExistingConvos(message.user);
+        controller.studio.run(bot, 'get_human_help', message.user, message.channel, message).catch(function (err) {
+            if (err) {
+                controller.studio.run(bot, 'default_fallback', message.user, message.channel, message);
+                debug('Botkit Studio: ', err);
+            }
+        });
+
+    });
+
+};
