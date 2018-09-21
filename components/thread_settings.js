60eee3faff8c92e0e26b690b9c025c8002b6d770
Ze Xuan Ong <zexuan@stashaway.com>
2018-09-21 19:23:15 -0400

    Initial commit. Setup infrastructure

---
+++ components/thread_settings.js
@@ -0,0 +1,35 @@
+var debug = require('debug')('botkit:thread_settings');
+
+module.exports = function(controller) {
+
+    debug('Configuring Facebook thread settings...');
+    controller.api.thread_settings.greeting("Hello! I am a friendly bot to help you buy and sell blocks!");
+    controller.api.thread_settings.get_started('get_started');
+    controller.api.thread_settings.menu([
+        {
+            "locale": "default",
+            "composer_input_disabled": false,
+            "call_to_actions": [    
+                {
+                    "type":"postback",
+                    "title":"Buy",
+                    "payload":"buy_block"
+                },
+                {
+                    "type":"postback",
+                    "title":"Sell",
+                    "payload":"sell_block"
+                },
+                {
+                    "type":"postback",
+                    "title":"Check Status",
+                    "payload":"check_status"
+                },
+                {
+                    "type":"postback",
+                    "title":"Give Feedback",
+                    "payload":"give_feedback"
+                }
+            ]
+        }]);
+}
