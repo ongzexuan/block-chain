6acbe42e68b00dee66c91453d50f7f0795cb4f9d
Ze Xuan Ong <zexuan@stashaway.com>
2018-09-22 00:51:06 -0400

    Fixed known bug issue where beforeThread doesn't work for default threads

--- skills/buy.js
+++ skills/buy.js
@@ -28,15 +28,22 @@ module.exports = function(controller) {
         bot.startConversation(message, function(err, convo) {
 
             // convo var defaults
-            convo.setVar('start_time','12:00pm');
-            convo.setVar('end_time','1:00pm');
-            convo.setVar('phone_number','1234567890');
+            convo.setVar('start_time', '12:00pm');
+            convo.setVar('end_time', '1:00pm');
+            convo.setVar('phone_number', '1234567890');
 
-            convo.beforeThread('default', function(convo, next) {
-                checkExistingOrder(convo.source_message.user, function(exists) {
-                   if (exists) {
-                       convo.gotoThread('order_already_exists')
-                   }
+            // Get start time
+            convo.addMessage({
+                text: 'Buying a block later, ok!',
+                action: 'start_time'
+            }, 'default');
+
+            convo.beforeThread('success', function (convo, next) {
+                console.log('before thread')
+                checkExistingOrder(convo.source_message.user, function (exists) {
+                    if (exists) {
+                        convo.gotoThread('order_already_exists')
+                    }
                 });
             });
 
@@ -49,7 +56,7 @@ module.exports = function(controller) {
             // Get start time
             convo.addMessage({
                 text: 'I\'m sorry I can\'t read that. Please enter the time in the following format: 10:30am',
-                action: 'default'
+                action: 'start_time'
             }, 'bad_start_time');
 
             convo.addMessage({
@@ -60,7 +67,7 @@ module.exports = function(controller) {
             convo.addQuestion('Please enter the start time in the following format: 10:30am', [
                 {
                     pattern: '^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9][ap]m$',
-                    callback: function(res, convo) {
+                    callback: function (res, convo) {
                         // TODO: Update Database with new start time
                         convo.setVar('start_time', convo.extractResponse('text'));
                         convo.gotoThread('good_start_time');
@@ -69,11 +76,11 @@ module.exports = function(controller) {
                 },
                 {
                     default: true,
-                    callback: function(res, convo) {
+                    callback: function (res, convo) {
                         convo.gotoThread('bad_start_time');
                     },
                 }
-            ], {key: 'text'}, 'default');
+            ], {key: 'text'}, 'start_time');
 
             // Get end time
             convo.addMessage({
@@ -89,7 +96,7 @@ module.exports = function(controller) {
             convo.addQuestion('Ok! Please also enter the end time in the same format: 10:30am', [
                 {
                     pattern: '^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9][ap]m$',
-                    callback: function(res, convo) {
+                    callback: function (res, convo) {
                         // TODO: Update Database with new end time
                         convo.setVar('end_time', convo.extractResponse('text'));
                         convo.gotoThread('good_end_time');
@@ -97,16 +104,16 @@ module.exports = function(controller) {
                 },
                 {
                     default: true,
-                    callback: function(res, convo) {
+                    callback: function (res, convo) {
                         convo.gotoThread('bad_end_time');
                     },
                 }
             ], {key: 'text'}, 'end_time');
 
             // Check phone number
-            convo.beforeThread('success', function(convo, next) {
-                checkPhoneNumber(convo.source_message.user, function(exists) {
-                    if (!exists) 
+            convo.beforeThread('success', function (convo, next) {
+                checkPhoneNumber(convo.source_message.user, function (exists) {
+                    if (!exists) {
                         convo.gotoThread('no_phone_number');
                     }
                 });
@@ -126,7 +133,7 @@ module.exports = function(controller) {
             convo.addQuestion('Please enter your phone number so that the seller can contact you when matched!', [
                 {
                     pattern: '^[0-9]{10}$',
-                    callback: function(res, convo) {
+                    callback: function (res, convo) {
                         // TODO: Update Database with new end time
                         convo.setVar('phone_number', convo.extractResponse('text'));
                         convo.gotoThread('good_phone_number');
@@ -134,7 +141,7 @@ module.exports = function(controller) {
                 },
                 {
                     default: true,
-                    callback: function(res, convo) {
+                    callback: function (res, convo) {
                         convo.gotoThread('bad_phone_number');
                     },
                 }
@@ -145,21 +152,37 @@ module.exports = function(controller) {
                 text: 'Ok! We\'ve placed a buy order for you at 10:30am today! We\'ll keep you posted when we find a' +
                 ' match for you!',
                 action: 'completed'
-            }, 'success')
-
+            }, 'success');
         });
     });
+
 
     // buy_block now
     controller.hears(['buy_now'], 'facebook_postback', function(bot, message) {
         console.log("Received a get_started postback message for buy_now!");
 
-        bot.startConversation(message, function(err, convo) {
+        bot.startConversation(message, function (err, convo) {
 
-            convo.beforeThread('default', function(convo, next) {
-                checkExistingOrder(convo.source_message.user, function(exists) {
+            // convo var defaults
+            convo.setVar('start_time', '12:00pm');
+            convo.setVar('end_time', '1:00pm');
+            convo.setVar('phone_number', '1234567890');
+
+            convo.addMessage({
+                text: 'Buying a block now, ok!',
+                action: 'success'
+            }, 'default');
+
+            convo.beforeThread('success', function (convo, next) {
+                checkExistingOrder(convo.source_message.user, function (exists) {
                     if (exists) {
-                        convo.gotoThread('order_already_exists')
+                        convo.gotoThread('order_already_exists');
+                    } else {
+                        checkPhoneNumber(convo.source_message.user, function(exists) {
+                           if (!exists) {
+                               convo.gotoThread('no_phone_number')
+                           }
+                        });
                     }
                 });
             });
@@ -170,72 +193,9 @@ module.exports = function(controller) {
                 action: 'completed'
             }, 'order_already_exists');
 
-            // Get start time
-            convo.addMessage({
-                text: 'I\'m sorry I can\'t read that. Please enter the time in the following format: 10:30am',
-                action: 'default'
-            }, 'bad_start_time');
-
-            convo.addMessage({
-                text: 'You gave a valid datetime string for the start_time',
-                action: 'end_time'
-            }, 'good_start_time');
-
-            convo.addQuestion('Please enter the start time in the following format: 10:30am', [
-                {
-                    pattern: '^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9][ap]m$',
-                    callback: function(res, convo) {
-                        // TODO: Update Database with new start time
-                        convo.gotoThread('good_start_time');
-                    }
-                },
-                {
-                    default: true,
-                    callback: function(res, convo) {
-                        convo.gotoThread('bad_start_time');
-                    },
-                }
-            ], {}, 'default');
-
-            // Get end time
-            convo.addMessage({
-                text: 'I\'m sorry I can\'t read that. Please enter the time in the following format: 10:30am',
-                action: 'end_time'
-            }, 'bad_end_time');
-
-            convo.addMessage({
-                text: 'You gave a valid datetime string for the end_time',
-                action: 'success'
-            }, 'good_end_time');
-
-            convo.addQuestion('Ok! Please also enter the end time in the same format: 10:30am', [
-                {
-                    pattern: '^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9][ap]m$',
-                    callback: function(res, convo) {
-                        // TODO: Update Database with new end time
-                        convo.gotoThread('good_end_time');
-                    }
-                },
-                {
-                    default: true,
-                    callback: function(res, convo) {
-                        convo.gotoThread('bad_end_time');
-                    },
-                }
-            ], {}, 'end_time');
-
-            // Check phone number
-            convo.beforeThread('success', function(convo, next) {
-                checkPhoneNumber(convo.source_message.user, function(exists) {
-                    if (!exists) {
-                        convo.gotoThread('no_phone_number');
-                    }
-                });
-            });
-
             // Get phone number
             convo.addMessage({
-                text: 'Ok so your phone number is 1234567890, thank you!',
+                text: 'Ok so your phone number is {{vars.phone_number}}, thank you!',
                 action: 'success'
             }, 'good_phone_number');
 
@@ -247,27 +207,27 @@ module.exports = function(controller) {
             convo.addQuestion('Please enter your phone number so that the seller can contact you when matched!', [
                 {
                     pattern: '^[0-9]{10}$',
-                    callback: function(res, convo) {
+                    callback: function (res, convo) {
                         // TODO: Update Database with new end time
+                        convo.setVar('phone_number', convo.extractResponse('text'));
                         convo.gotoThread('good_phone_number');
                     }
                 },
                 {
                     default: true,
-                    callback: function(res, convo) {
+                    callback: function (res, convo) {
                         convo.gotoThread('bad_phone_number');
                     },
                 }
-            ], {}, 'no_phone_number');
+            ], {key: 'text'}, 'no_phone_number');
 
             // Success confirmation
             convo.addMessage({
-                text: 'Ok! We\'ve placed a buy order for you at 10:30am today! We\'ll keep you posted when we find a' +
+                text: 'Ok! We\'ve placed a buy order for you now! We\'ll keep you posted when we find a' +
                 ' match for you!',
                 action: 'completed'
             }, 'success')
 
         });
     });
-
 };
