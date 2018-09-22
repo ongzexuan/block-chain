dd4a6fd09a99337b2ea2511591f6824e0099932e
Ze Xuan Ong <zexuan@stashaway.com>
2018-09-21 22:29:48 -0400

    Fixed undefined attachment bug

--- skills/buy.js
+++ skills/buy.js
@@ -26,10 +26,6 @@ module.exports = function(controller) {
                 }
             ], {}, 'default');
         });
-
-        bot.reply(message, {
-            attachment: attachment
-        });
     });
 
 };
